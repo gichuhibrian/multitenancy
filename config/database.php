52552793c3437c68565db5c65503162984173aeb
Brian Chege <brianchegegichuhi@gmail.com>
2021-10-18 06:53:14 +0100

    Added database seeders

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'tenant' => [
+            'driver' => 'mysql',
+            'url' => env('TENANCY_DATABASE_URL'),
+            'host' => env('TENANCY_DB_HOST', '127.0.0.1'),
+            'port' => env('TENANCY_DB_PORT', '3306'),
+            'database' => env('TENANCY_DB_DATABASE', 'forge'),
+            'username' => env('TENANCY_DB_USERNAME', 'forge'),
+            'password' => env('TENANCY_DB_PASSWORD', ''),
+            'unix_socket' => env('TENANCY_DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(SystemUsersSeeder::class);
-        $this->calss(TenantDatabaseSeeder::class);
+        $this->call(TenantUsersSeeder::class);
     }
 }
