fd23fc06c285c60f6bb04ad519661170a2f9ad80
Brian Chege <brianchegegichuhi@gmail.com>
2021-10-18 06:16:49 +0100

    Added database connection and created tenant and system users

--- app/Models/System/User.php
+++ app/Models/System/User.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\System;
 
+use Hyn\Tenancy\Traits\UsesSystemConnection;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +11,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, UsesSystemConnection;
 
     /**
      * The attributes that are mass assignable.
